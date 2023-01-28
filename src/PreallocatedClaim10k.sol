e1cc6b9d574aec0c2b1fc1fb9b702f1441784e78
HrikB <hrik.bhowal@gmail.com>
2023-01-27 18:50:59 -0800

    add a version of the contract that parameterizes max supply

--- src/PreallocatedClaim10k.sol
+++ src/PreallocatedClaim10k.sol
@@ -7,21 +7,18 @@ import {ERC721} from "solmate/tokens/ERC721.sol";
  * @dev tokenIds must be a set of sequential integers from 0 to MAX_SUPPLY - 1
  */
 contract PreallocatedClaim is ERC721 {
-    uint256 internal maxSupply;
+    uint256 internal constant MAX_SUPPLY = 10000;
     uint256 internal constant PREALLOCATION_PERIOD = 2 days;
 
     uint256 private _preallocationEnd; //timestamp
-    uint256 private _randomClaimsCount;
-    uint16[] private _preallocatedClaims;
-    uint16[] private _randomClaimsLeft;
+    uint256 private _randomClaimsCount = MAX_SUPPLY;
+    uint16[MAX_SUPPLY] private _preallocatedClaims;
+    uint16[MAX_SUPPLY] private _randomClaimsLeft;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _maxSupply
-    ) ERC721(_name, _symbol) {
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {
         _preallocationEnd = block.timestamp + PREALLOCATION_PERIOD;
-        maxSupply = _maxSupply;
     }
 
     function mint(address to, uint16 claimId) external {
@@ -30,13 +27,13 @@ contract PreallocatedClaim is ERC721 {
             "Preallocation period has ended"
         );
         // Cases to consider:
-        // 1) claimId >= maxSupply
+        // 1) claimId >= MAX_SUPPLY
         // 2) claimId is already claimed; mint will fail because already minted
         // 3) claimId = tokensLeft (last token in array)
         // 4) claimId = 0
         // 5) claimId < tokensLeft (general case)
         require(
-            claimId < maxSupply,
+            claimId < MAX_SUPPLY,
             "PreallocatedClaim: claimId is greater than the maximum supply"
         );
 

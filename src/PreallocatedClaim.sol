d4f26b8a6bd5a69bf062c599cc6642ad6d41555e
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-01-28 16:26:11 -0800

    unit tests and fuzz test

--- src/PreallocatedClaim.sol
+++ src/PreallocatedClaim.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {ERC721} from "solmate/tokens/ERC721.sol";
+import "forge-std/console.sol";
 
 /**
  * @dev tokenIds must be a set of sequential integers from 0 to MAX_SUPPLY - 1
@@ -9,9 +10,8 @@ import {ERC721} from "solmate/tokens/ERC721.sol";
 contract PreallocatedClaim is ERC721 {
     uint256 internal immutable maxSupply;
 
-    uint256 public randomClaimsCount;
+    uint256 public claimsLeftCount;
 
-    uint16[] internal _preallocatedClaims;
     uint16[] internal _randomClaimsLeft;
 
     uint256 private _preallocationEnd; //timestamp
@@ -24,6 +24,9 @@ contract PreallocatedClaim is ERC721 {
     ) ERC721(_name, _symbol) {
         _preallocationEnd = block.timestamp + _preallocationPeriod;
         maxSupply = _maxSupply;
+        claimsLeftCount = _maxSupply;
+
+        _randomClaimsLeft = new uint16[](_maxSupply);
     }
 
     function mint(address to, uint16 claimId) external {
@@ -31,6 +34,7 @@ contract PreallocatedClaim is ERC721 {
             block.timestamp < _preallocationEnd,
             "Preallocation period has ended"
         );
+
         // Cases to consider:
         // 1) claimId >= maxSupply
         // 2) claimId is already claimed; mint will fail because already minted
@@ -42,17 +46,32 @@ contract PreallocatedClaim is ERC721 {
             "PreallocatedClaim: claimId is greater than the maximum supply"
         );
 
-        uint256 tokensLeft = --randomClaimsCount; // Amount of tokens left after this transaction's success
-        if (claimId <= tokensLeft)
-            _randomClaimsLeft[claimId] = uint16(tokensLeft);
-        else
-            _randomClaimsLeft[_preallocatedClaims[claimId]] = uint16(
-                tokensLeft
-            );
+        uint256 claimIdEncode = claimId == 0 ? maxSupply : claimId;
 
-        _preallocatedClaims[tokensLeft] = claimId;
+        uint256 tokensLeft = --claimsLeftCount; // Amount of tokens left after this transaction's success
+        if (claimId <= tokensLeft) {
+            _randomClaimsLeft[claimId] = uint16(tokensLeft);
+            _randomClaimsLeft[tokensLeft] = uint16(claimIdEncode);
+        } else {
+            uint16 indexToSwitch = _randomClaimsLeft[claimId];
+            uint16 indexToSwitchDecode = indexToSwitch == maxSupply
+                ? 0
+                : indexToSwitch;
+            _randomClaimsLeft[indexToSwitchDecode] = uint16(
+                getArrayValue(uint16(tokensLeft))
+            );
+            _randomClaimsLeft[tokensLeft] = uint16(indexToSwitch);
+        }
 
         _safeMint(to, claimId);
+    }
+
+    function getArrayValue(uint16 index) public view returns (uint16) {
+        uint16 val = _randomClaimsLeft[index];
+
+        if (val == 0) return index;
+        if (val == maxSupply) return 0;
+        return val;
     }
 
     function tokenURI(uint256 tokenId)
--- test/PreallocatedClaim.t.sol
+++ test/PreallocatedClaim.t.sol
@@ -12,26 +12,249 @@ contract PreallocatedClaimExposed is PreallocatedClaim {
         uint256 _preallocationPeriod
     ) PreallocatedClaim(_name, _symbol, _maxSupply, _preallocationPeriod) {}
 
-    function preallocatedClaims(uint256 index) public view returns (uint16) {
-        return _preallocatedClaims[index];
-    }
-
     function randomClaimsLeft(uint256 index) public view returns (uint16) {
         return _randomClaimsLeft[index];
     }
 }
 
 contract PreallocatedClaimTest is Test {
-    PreallocatedClaim public preallocatedClaim;
+    PreallocatedClaimExposed public preallocatedClaim;
     uint256 private removedId1;
     uint256 private removedId2;
     uint256 private removedId3;
     uint256 private removedId4;
     uint256 private removedId5;
+    uint256 private constant totalSupply = 10;
+
+    address user = vm.addr(1);
 
     function setUp() public {
-        preallocatedClaim = new PreallocatedClaim("Test", "TST", 10, 2 days);
+        preallocatedClaim = new PreallocatedClaimExposed(
+            "Test",
+            "TST",
+            totalSupply,
+            2 days
+        );
+    }
+
+    function testZeroIndexMint() external {
+        uint256[] memory expectedArr = new uint256[](totalSupply);
+        for (uint256 i = 0; i < expectedArr.length; i++) {
+            expectedArr[i] = i;
+        }
+
+        uint16 tokenIdToMint = 0;
+
+        vm.expectRevert(bytes("NOT_MINTED"));
+        preallocatedClaim.ownerOf(tokenIdToMint);
+        checkClaimsArrayState(expectedArr);
+
+        preallocatedClaim.mint(user, tokenIdToMint);
+
+        expectedArr[tokenIdToMint] = totalSupply - 1;
+        expectedArr[totalSupply - 1] = tokenIdToMint;
+        checkClaimsArrayState(expectedArr);
+    }
+
+    function testMiddleIndexMint() external {
+        uint256[] memory expectedArr = new uint256[](totalSupply);
+        for (uint256 i = 0; i < expectedArr.length; i++) {
+            expectedArr[i] = i;
+        }
+
+        uint16 tokenIdToMint = 5;
+
+        vm.expectRevert(bytes("NOT_MINTED"));
+        preallocatedClaim.ownerOf(tokenIdToMint);
+        checkClaimsArrayState(expectedArr);
+
+        preallocatedClaim.mint(user, tokenIdToMint);
+
+        expectedArr[tokenIdToMint] = totalSupply - 1;
+        expectedArr[totalSupply - 1] = tokenIdToMint;
+        checkClaimsArrayState(expectedArr);
+    }
+
+    function testFinalIndexMint() external {
+        uint256[] memory expectedArr = new uint256[](totalSupply);
+        for (uint256 i = 0; i < expectedArr.length; i++) {
+            expectedArr[i] = i;
+        }
+
+        uint16 tokenIdToMint = uint16(totalSupply) - 1;
+
+        vm.expectRevert(bytes("NOT_MINTED"));
+        preallocatedClaim.ownerOf(tokenIdToMint);
+        checkClaimsArrayState(expectedArr);
+
+        preallocatedClaim.mint(user, tokenIdToMint);
+
+        expectedArr[tokenIdToMint] = totalSupply - 1;
+        expectedArr[totalSupply - 1] = tokenIdToMint;
+        checkClaimsArrayState(expectedArr);
+    }
+
+    function testMintAll() external {
+        uint256[] memory expectedArr = new uint256[](totalSupply);
+        for (uint256 i = 0; i < expectedArr.length; i++) {
+            expectedArr[i] = i;
+        }
+        checkClaimsArrayState(expectedArr);
+
+        for (uint16 i = 0; i < totalSupply; i++) {
+            uint16 tokenIdToMint = i;
+
+            uint256 countLeft = preallocatedClaim.claimsLeftCount();
+
+            vm.expectRevert(bytes("NOT_MINTED"));
+            preallocatedClaim.ownerOf(tokenIdToMint);
+
+            uint16 cacheCountLeftArrVal = uint16(
+                preallocatedClaim.getArrayValue(uint16(countLeft - 1))
+            );
+
+            preallocatedClaim.mint(user, tokenIdToMint);
+            --countLeft;
+
+            if (tokenIdToMint <= countLeft) {
+                expectedArr[tokenIdToMint] = countLeft;
+                expectedArr[countLeft] = uint16(tokenIdToMint);
+            } else {
+                uint16 indexToSwitch = uint16(expectedArr[tokenIdToMint]);
+                expectedArr[indexToSwitch] = cacheCountLeftArrVal;
+                expectedArr[countLeft] = indexToSwitch;
+            }
+            checkClaimsArrayState(expectedArr);
+        }
+    }
+
+    function testMintAllReverse() external {
+        uint256[] memory expectedArr = new uint256[](totalSupply);
+        for (uint256 i = 0; i < expectedArr.length; i++) {
+            expectedArr[i] = i;
+        }
+        checkClaimsArrayState(expectedArr);
+
+        // i-- underflows
+        for (int16 i = (int16(uint16(totalSupply)) - 1); i >= 0; i--) {
+            uint16 tokenIdToMint = uint16(i);
+
+            uint256 countLeft = preallocatedClaim.claimsLeftCount();
+
+            vm.expectRevert(bytes("NOT_MINTED"));
+            preallocatedClaim.ownerOf(tokenIdToMint);
+
+            uint16 cacheCountLeftArrVal = uint16(
+                preallocatedClaim.getArrayValue(uint16(countLeft - 1))
+            );
+
+            preallocatedClaim.mint(user, tokenIdToMint);
+            --countLeft;
+
+            if (tokenIdToMint <= countLeft) {
+                expectedArr[tokenIdToMint] = countLeft;
+                expectedArr[countLeft] = uint16(tokenIdToMint);
+            } else {
+                uint16 indexToSwitch = uint16(expectedArr[tokenIdToMint]);
+                expectedArr[indexToSwitch] = cacheCountLeftArrVal;
+                expectedArr[countLeft] = indexToSwitch;
+            }
+            checkClaimsArrayState(expectedArr);
+        }
+    }
+
+    function testMintAllRandom() external {
+        uint256[] memory expectedArr = new uint256[](totalSupply);
+        for (uint256 i = 0; i < expectedArr.length; i++) {
+            expectedArr[i] = i;
+        }
+        checkClaimsArrayState(expectedArr);
+
+        uint16[10] memory mintOrder = [uint16(3), 2, 4, 9, 7, 5, 8, 1, 6, 0];
+
+        for (uint16 i = 0; i < mintOrder.length; i++) {
+            uint16 tokenIdToMint = mintOrder[i];
+            uint256 countLeft = preallocatedClaim.claimsLeftCount();
+
+            vm.expectRevert(bytes("NOT_MINTED"));
+            preallocatedClaim.ownerOf(tokenIdToMint);
+
+            uint16 cacheCountLeftArrVal = uint16(
+                preallocatedClaim.getArrayValue(uint16(countLeft - 1))
+            );
+
+            preallocatedClaim.mint(user, tokenIdToMint);
+            --countLeft;
+
+            if (tokenIdToMint <= countLeft) {
+                expectedArr[tokenIdToMint] = countLeft;
+                expectedArr[countLeft] = uint16(tokenIdToMint);
+            } else {
+                uint16 indexToSwitch = uint16(expectedArr[tokenIdToMint]);
+                expectedArr[indexToSwitch] = cacheCountLeftArrVal;
+                expectedArr[countLeft] = indexToSwitch;
+            }
+            checkClaimsArrayState(expectedArr);
+        }
+    }
+
+    function testMintAllFuzz(uint16[10] memory arr) external {
+        uint256[] memory expectedArr = new uint256[](totalSupply);
+        for (uint256 i = 0; i < expectedArr.length; i++) {
+            expectedArr[i] = i;
+        }
+        checkClaimsArrayState(expectedArr);
+
+        uint16[] memory allTokenIds = new uint16[](10);
+        for (uint16 i = 0; i < allTokenIds.length; i++) {
+            allTokenIds[i] = i;
+        }
+
+        for (uint16 i = 0; i < allTokenIds.length; i++) {
+            uint256 countLeft = preallocatedClaim.claimsLeftCount();
+            uint16 indexToMint = arr[i] % uint16(countLeft);
+            uint16 tokenIdToMint = allTokenIds[indexToMint];
+            allTokenIds[indexToMint] = allTokenIds[countLeft - 1];
+            console.log("tokenIdToMint", tokenIdToMint);
+
+            vm.expectRevert(bytes("NOT_MINTED"));
+            preallocatedClaim.ownerOf(tokenIdToMint);
+
+            uint16 cacheCountLeftArrVal = uint16(
+                preallocatedClaim.getArrayValue(uint16(countLeft - 1))
+            );
+
+            preallocatedClaim.mint(user, tokenIdToMint);
+            --countLeft;
+
+            if (tokenIdToMint <= countLeft) {
+                expectedArr[tokenIdToMint] = countLeft;
+                expectedArr[countLeft] = uint16(tokenIdToMint);
+            } else {
+                uint16 indexToSwitch = uint16(expectedArr[tokenIdToMint]);
+                expectedArr[indexToSwitch] = cacheCountLeftArrVal;
+                expectedArr[countLeft] = indexToSwitch;
+            }
+            checkClaimsArrayState(expectedArr);
+        }
     }
 
     /* INVARIANTS */
+    function checkClaimsArrayState(uint256[] memory expectedState) private {
+        uint256 claimsLeft = preallocatedClaim.claimsLeftCount();
+
+        for (uint16 i = 0; i < expectedState.length; i++) {
+            if (claimsLeft == 0 && i == 0) console.log("---------");
+            console.log(
+                expectedState[i],
+                preallocatedClaim.getArrayValue(i),
+                preallocatedClaim.randomClaimsLeft(i)
+            );
+            if (i + 1 == claimsLeft) console.log("---------");
+            assertEq(expectedState[i], preallocatedClaim.getArrayValue(i));
+        }
+        console.log();
+    }
+
+    /* UTILS */
 }
